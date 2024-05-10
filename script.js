a69534de27a8a961fdd6ef6b92543d26589eb68c
bcrhbrhcdb <apet2804@mpsedu.org>
2024-05-10 12:45:05 -0500

    a

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-// Import the necessary Firebase modules
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { getDatabase, ref, set, get, child, update, remove, onValue } from "firebase/database";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDisnhcjYmvZc2y9-toeWWKHq9nHYb8Fn4",
   authDomain: "chatroom-50dfb.firebaseapp.com",
@@ -16,8 +18,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const database = getDatabase(app);
+const analytics = getAnalytics(app);
+
 
 // Sign-up function
 async function signUpWithEmail(email, password) {
