0bb7cb8453d96dfcb9aa92e99ca6be17e9c25e10
bcrhbrhcdb <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-05-13 16:27:20 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, collection } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -20,4 +20,4 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { auth, db };
+export { auth, db, collection };
