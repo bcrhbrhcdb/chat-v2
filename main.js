178243c1843943dc3e6ed9cad4fa93f3406b41f4
bcrhbrhcdb <123518949+bcrhbrhcdb@users.noreply.github.com>
2024-05-14 08:47:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, sendPasswordResetEmail, updateProfile, signInAnonymously } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-auth.js";
-import { auth, db, collection } from './firebase.js';
+import { auth, db } from './firebase.js';
 import { initializeChat } from './chat.js';
 import { doc, setDoc, updateDoc, collection, addDoc, query, orderBy, onSnapshot, getDocs, where, writeBatch } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-firestore.js";
 import { sendVerificationEmail } from './utils.js';
